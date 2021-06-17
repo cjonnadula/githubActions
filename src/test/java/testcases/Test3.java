1517b52238f2fbe181775a3e7918b23a2c02f582
arpitaTFACT <73965469+arpitaTFACT@users.noreply.github.com>
2021-06-17 15:25:10 -0400

    Update Test3.java

--- src/test/java/testcases/Test3.java
+++ src/test/java/testcases/Test3.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
 public class Test3{
 	
