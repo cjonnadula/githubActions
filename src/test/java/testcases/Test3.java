78bfda730a6c450234370d94a167569e5334acb3
arpitaTFACT <73965469+arpitaTFACT@users.noreply.github.com>
2021-06-17 15:18:12 -0400

    Update Test3.java

--- src/test/java/testcases/Test3.java
+++ src/test/java/testcases/Test3.java
@@ -6,13 +6,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import common.WebDriverBase;
-
-public class Test3 extends WebDriverBase{
+public class Test3{
 	
    
-    public void testSomeLibraryMethod() throws InterruptedException{
-    //System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\ChromeDriver\\chromedriver.exe");  
+    	public void testChrome1()
+    {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\ChromeDriver\\chromedriver.exe");  
 		WebDriver driver=new ChromeDriver();  
 		
         driver.get("http://www.facebook.com");
@@ -20,7 +19,63 @@ public class Test3 extends WebDriverBase{
         WebElement mailID = driver.findElement(By.id("email"));
         mailID.sendKeys("test1@gmail.com");
         
-        driver.close();
+        driver.quit();
+	}
+
+	public void testChrome2()
+    {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\ChromeDriver\\chromedriver.exe");  
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
 	
-}
+		WebDriver driver = new ChromeDriver(options);
+        driver.navigate().to("http://www.facebook.com");
+        driver.manage().window().maximize();
+        
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+        
+        WebElement mailID = driver.findElement(By.id("email"));
+        mailID.sendKeys("testingDemo@gmail.com");
+        
+        driver.quit();
+	}
+	
+	@Test
+	public void testChrome3()
+    {
+		WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        
+        driver.navigate().to("http://www.facebook.com");
+        driver.manage().window().maximize();
+        
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+        
+        WebElement mailID = driver.findElement(By.id("email"));
+        mailID.sendKeys("test1@gmail.com");
+        
+        driver.quit();
+    }
+	
+	public void testChrome4()
+    {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+        
+        driver.navigate().to("http://www.facebook.com");
+        driver.manage().window().maximize();
+        
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+        
+        WebElement mailID = driver.findElement(By.id("email"));
+        mailID.sendKeys("test1@gmail.com");
+        
+        driver.quit();
+    }
 }   
